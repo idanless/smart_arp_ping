3ea675572c8984364cf5c4e8b07c69b912e6a80b
idanless <31201417+idanless@users.noreply.github.com>
2019-11-21 23:54:10 +0200

    Update arping.sh

--- arping.sh
+++ arping.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
+interface=enp1s0
+#/24
+ip_addr=10.100.102
+thread=30
+output_file=arp_data.csv
+
 send_arp_ping()
+
 {
-url=$(gssdp-discover -i eno1 --timeout=1 | grep "Location:" | cut -d ' ' -f4 | grep  "$1"  |sort -u)
-check_upnp
-data_ping=$(arping -w 1 -c 1  -I eno1  $1 | grep Uni | cut -c 19-60 | sed 's/[\t ]+/,/g'  | cut -d ' ' -f2-10 |  tr -d {}[] | ts | awk '{$1=$1}1' OFS="," | sed 's/ms//'|sed 's/m//')
-check_OUI
+
+IP=$(echo $1)
+data_ping=$(arping -w 1 -c 1  -I $interface  $IP | grep Uni | cut -c 19-60 | sed 's/[\t ]+/,/g'  | cut -d ' ' -f2-10 |  tr -d {}[] | ts | awk '{$1=$1}1' OFS="," | sed 's/ms//' | sed 's/m//' )
 if [ -z $data_ping ]
 then
      echo "empty" >> /dev/null
 
 else
-	echo $data_ping,$model,$oui
-
+	#echo $data_ping,$model,$oui
+	url=$(gssdp-discover -i $interface --timeout=3 | grep "Location:" | cut -d ' ' -f4 | grep -w  "$1" |grep -E 'Desc|desc' |sort -u|head -n1)
+	check_OUI
+	check_bonjour
+	check_upnp
+	echo $data_ping,$model,$mdns,$oui>>$output_file
      
 fi
 
@@ -24,7 +34,7 @@ check_arp_ping()
 while true ;
 do
 count=$(ps -aux | grep "arping -w 1" | grep -v "color=auto" | wc -l)
-if [ $count -lt 50 ]
+if [ $count -lt $thread ]
 then
 break 
 fi
@@ -66,9 +76,27 @@ fi
 
 }
 
+
+check_bonjour()
+{
+
+bonjour_host=$(avahi-resolve  --address $IP |  awk '{$1=$1}1' OFS=","|cut -d ',' -f2 | cut -d '.' -f1)
+if [ -z $bonjour_host ]
+then
+      mdns=$(echo "N\A")
+
+else
+        mdns=$(echo $bonjour_host)
+
+fi
+
+
+}
+
+
 for i  in {1..254} 
  do 
 #echo test 192.168.0.$i
 check_arp_ping
-send_arp_ping 192.168.1.$i &
+send_arp_ping $ip_addr.$i &
 done
